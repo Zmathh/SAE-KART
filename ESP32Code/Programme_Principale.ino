ba6084d518de964181e6f3e943beed906a64cb80
juanpedrosis <arno.stevens7@outlook.com>
2024-01-30 14:44:06 +0100

    modif

--- ESP32Code/Programme_Principale.ino
+++ ESP32Code/Programme_Principale.ino
@@ -1,1 +1,16 @@
 #include <Arduino.h>
+
+#define FREIN 36
+#define ACCEL 39
+
+pinMode(FREIN, INPUT);
+pinMode(ACCEL, INPUT);
+
+void setup() {
+    Serial.begin(115200);
+    
+}
+
+void loop() {
+
+}
