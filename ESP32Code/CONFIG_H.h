480f8c760c5fd5450a80c080f5a41350db981150
ivannopolis <marcoivan8@gmail.com>
2024-01-30 16:29:50 +0100

    define des varriable

--- ESP32Code/CONFIG_H.h
+++ ESP32Code/CONFIG_H.h
@@ -4,16 +4,26 @@
 
 #define VITESSE_SERIAL 115200
 
-
-
-
-
 // PINs
 
-#define FREIN =
-#define ACELL = 
-
-
+#define FREIN = 26;
+#define ACELL = 25;
+#define TensionGrandBat = 13;
+#define TensionPetiteBat = 17;
+#define LedSignal = 23;
+#define I2C_SCL = 22;
+#define i2C_SDA = 21;
+#define uWire_SI_O = 19;
+#define uWire_CLK = 18;
+#define GPS_Tx = 17;
+#define GPS_Rx = 16;
+#define Led_Alarme = 15;
+#define CS_STD = 0;
+#define CS_CKL = 2;
+#define CS_DAT = 4:
+#define RESET = 5;
+#define INT = 32;
+#define Vistesse = 33;
 
 // Add other configurations or declarations if needed
 
