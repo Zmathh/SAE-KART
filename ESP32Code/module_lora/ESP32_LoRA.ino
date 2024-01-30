eeacd542575bdc34ecf731f53b2cd58363b6df79
juanpedrosis <arno.stevens7@outlook.com>
2024-01-30 16:38:11 +0100

    lora

--- ESP32Code/module_lora/ESP32_LoRA.ino
+++ ESP32Code/module_lora/ESP32_LoRA.ino
@@ -1,27 +1,28 @@
 #include "module_lora.h"
 #include <SoftwareSerial.h>
-SoftwareSerial SoftSerial(8, 9);
-unsigned char buffer[64];                   // buffer array for data receive over serial port
-int count=0;                                // counter for buffer array
 
 
-const uint8_t dataPacketSize = sizeof(float) * 7 + sizeof(float) * 2; // Total size for 7 floats and 2 additional floats for latitude and longitude
-uint8_t dataPacket[dataPacketSize]; // Array to store the data to be sent
+SoftwareSerial SoftSerial(17, 16);
+unsigned char buffer[64];                  
+int count=0;                                
+
+
+const uint8_t dataPacketSize = sizeof(float) * 7 + sizeof(float) * 2; 
+uint8_t dataPacket[dataPacketSize];
 
 CModuleLoRa* pModuleLoRa = NULL;
-float floatsToSend[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0}; // Replace these with your actual float values
+float floatsToSend[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0}; 
 float latitude = 48.4423344;
 float longitude = 1.496218;
 
 void setup() {
-  SoftSerial.begin(9600);                 // the SoftSerial baud rate
+  SoftSerial.begin(9600);                 
   Serial.begin(9600); 
   delay(3000);
-  //while(!Serial);
   Serial.println("*** Started ***");
 
   pModuleLoRa = CModuleLoRa::GetInstance();
-  pModuleLoRa->init(44, 43);
+  pModuleLoRa->init(16, 17);
   pModuleLoRa->setConfig();
   pModuleLoRa->getConfig();
   pModuleLoRa->getInfo();
@@ -33,16 +34,15 @@ void loop() {
   for (int i = 0; i < 7; i++) {
     floatsToSend[i] += 1.0;
   }
-  // Prepare the data to be sent
   latitude = latitude + 0.00001;
   longitude = longitude + 0.00001;
   
-  // Copy the float values and latitude/longitude into the dataPacket array
+
   memcpy(dataPacket, floatsToSend, sizeof(float) * 7);
   memcpy(dataPacket + sizeof(float) * 7, &latitude, sizeof(float));
   memcpy(dataPacket + sizeof(float) * 7 + sizeof(float), &longitude, sizeof(float));
 
-  // Send the data
+
   pModuleLoRa->radioTX(dataPacket, dataPacketSize);
 }
 
