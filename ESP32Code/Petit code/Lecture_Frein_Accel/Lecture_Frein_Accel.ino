893aec01e8ad854d48a0fafe72b2288e1066c771
juanpedrosis <arno.stevens7@outlook.com>
2024-01-30 15:07:45 +0100

    freinaccel

--- ESP32Code/Petit code/Lecture_Frein_Accel/Lecture_Frein_Accel.ino
+++ ESP32Code/Petit code/Lecture_Frein_Accel/Lecture_Frein_Accel.ino
@@ -1,5 +1,6 @@
 // MonCapteurs.cpp
 #include "Lecture_Frein_Accel.h"
+#include CONFIG_H.h
 
 Lecture_Frein_Accel::Lecture_Frein_Accel(int pinFrein, int pinAccel) {
     pinFrein_ = pinFrein;
@@ -7,7 +8,7 @@ Lecture_Frein_Accel::Lecture_Frein_Accel(int pinFrein, int pinAccel) {
 }
 
 void Lecture_Frein_Accel::setup() {
-    Serial.begin(115200);
+    Serial.begin(VITESSE_SERIAL);
     pinMode(pinFrein_, INPUT);
     pinMode(pinAccel_, INPUT);
 }
@@ -15,11 +16,5 @@ void Lecture_Frein_Accel::setup() {
 void Lecture_Frein_Accel::lireCapteurs() {
     Fr_Prcent = (int)((float)(((analogRead(pinFrein_) * 3.3) / 4095) / 3.3) * 100);
     Ac_Prcent = (int)((float)(((analogRead(pinAccel_) * 3.3) / 4095) / 3.3) * 100);
-
-    Serial.print("Frein : ");
-    Serial.println(Fr_Prcent);
-    Serial.print("Accel : ");
-    Serial.println(Ac_Prcent);
-
     delay(500);
 }
