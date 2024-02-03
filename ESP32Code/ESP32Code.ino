06e31e633e86bb8d2e8a1d299a78fc220aafa028
Arno STEVENS <arno.stevens7@outlook.com>
2024-02-03 19:04:43 +0100

    if endif activate

--- ESP32Code/ESP32Code.ino
+++ ESP32Code/ESP32Code.ino
@@ -1,135 +1,150 @@
 #include "CONFIG_H.h"
 
-// Var LoRa
+#define Activate_Serial 0
+#define Activate_GPS 0
+#define Activate_ShiftReg 0
+#define Activate_Temperature 0
+#define Activate_ACCEL_FREIN 0
+#define Activate_LoRa 0
+#define Activate_Ecran 0
+#define Activate_FREQ 0
 
-// const uint8_t dataPacketSize = sizeof(float) * 13 + sizeof(float) * 2; 
-// uint8_t dataPacket[dataPacketSize];
+#if Activate_Ecran == 1
+LiquidCrystal_I2C lcd(0x27,20,4);
+#endif
 
-// LiquidCrystal_I2C lcd(0x27,20,4);
+#if Activate_LoRa == 1
+const uint8_t dataPacketSize = sizeof(float) * 13 + sizeof(float) * 2; 
+uint8_t dataPacket[dataPacketSize];
+CModuleLoRa* pModuleLoRa = NULL;
+float data[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0};
+float latitude = 48.4423344;
+float longitude = 1.496218;
+#endif
 
-// //-------------------
-// // Variable Numero de port
-// //-------------------
+#if Activate_ACCEL_FREIN == 1
+Lecture_Frein_Accel lesLectures;
+#endif
 
-// pinMode(SC,OUTPUT);
-// pinMode(CSN,OUTPUT);
-// pinMode(SIO,INPUT);
+#if Activate_ShiftReg == 1
+pinMode(SC,OUTPUT);
+pinMode(CSN,OUTPUT);
+pinMode(SIO,INPUT);
+shiftReg shiftReg(CS_DAT, CS_CLK, CS_STRB);
+#endif
 
-// //-------------------
-// // Variables générales
-// //-------------------
+#if Activate_Temperature == 1
+LectTemp temperatureSensor(CS_CKL, CS_DAT, CS_STD); //// Vérifier les PINs
+#endif
 
-// Lecture_Frein_Accel lesLectures;
-
-// shiftReg shiftReg(CS_DAT, CS_CLK, CS_STRB);
-
-// LectTemp temperatureSensor(CS_CKL, CS_DAT, CS_STD); //// Vérifier les PINs
-
-// CModuleLoRa* pModuleLoRa = NULL;
-
-// GPS gps(GPS_Tx, GPS_Rx);
-
-// float data[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0};
-// float latitude = 48.4423344;
-// float longitude = 1.496218;
-
-//
+#if Activate_GPS == 1
+GPS gps(GPS_Tx, GPS_Rx);
+#endif
 
 void setup()
 {
-    //     //-------Serial----------------
-    // Serial.begin(VITESSE_SERIAL_DEBUG);  // Initialisation de la vitesse de la liaison série
+    #if Activate_Serial == 1
+    Serial.begin(VITESSE_SERIAL_DEBUG);  // Initialisation de la vitesse de la liaison série
+    #endif
 
-    //     //-------GPS-------------------
-    // gps.begin();
+    #if Activate_GPS == 1
+    gps.begin();
+    #endif
 
-    //     //-------shiftReg-----------
-    // shiftReg.setup();
+    #if Activate_ShiftReg == 1
+    shiftReg.setup();
+    #endif
 
-    //     //-------temperature-----------
-    //     temperatureSensor.begin();
+    #if Activate_Temperature == 1
+    temperatureSensor.begin();
+    #endif
 
-    //     ////-------Accelero/Frein--------
-    //     lesLectures.begin();  // Initialisation des capteurs
+    #if Activate_ACCEL_FREIN == 1
+    lesLectures.begin();  // Initialisation des capteurs
+    #endif
 
-    // Initialisation LoRa
-    // pModuleLoRa = CModuleLoRa::GetInstance();
-    // pModuleLoRa->init(16, 17);
-    // pModuleLoRa->setConfig();
-    // pModuleLoRa->getConfig();
-    // pModuleLoRa->getInfo();
+    #if Activate_LoRa == 1
+    pModuleLoRa = CModuleLoRa::GetInstance();
+    pModuleLoRa->init(16, 17);
+    pModuleLoRa->setConfig();
+    pModuleLoRa->getConfig();
+    pModuleLoRa->getInfo();
+    #endif
 
-    //     // Initialisation ECRAN
+    #if Activate_FREQ == 1
+    MFreq.setup();
+    #endif
 
-    //     // Initialisation MFREQUENCE
-    // MFreq.setup();
-
-    //     // Initialisation Ecran
-    //  ecran.setup();
+    #if Activate_Ecran == 1
+    ecran.setup();
+    #endif
 }
-// //----------------------------------------------------------------
+
 void loop()
 {
-    //-----------GPS---------------
-    // gps.readData();
-    // Serial.print("Latitude: ");
-    // Serial.print(gps.latitude, 6);
-    // Serial.print(" Longitude: ");
-    // Serial.println(gps.longitude, 6);
-    // gps.flushBuffer();
+    #if Activate_GPS == 1
+    gps.readData();
+    Serial.print("Latitude: ");
+    Serial.print(gps.latitude, 6);
+    Serial.print(" Longitude: ");
+    Serial.println(gps.longitude, 6);
+    gps.flushBuffer();
+    #endif
+
+    #if Activate_ShiftReg == 1
+    shiftReg.selectionTemp();
+    #endif
     
-    //----------shiftReg-----------
-
-    // shiftReg.selectionTemp();
-
-    //-------temperature-----------
-    //     Mesure_temp(Lect, Somme);
-
-    //-------Accelero/Frein--------
-    //     Serial.print("Frein : ");
-    //     Serial.println(lesLectures.readFrein());
-    //     Serial.print("Accel : ");
-    //     Serial.println(lesLectures.readAccel());
-    //     delay(500);
-    //     Serial.println(lectureFreinAccel.getFr_Prcent());
-        // Serial.println(lectureFreinAccel.getAc_Prcent());
-
-    //-------Envoi-LORA--------
-        // for (int i = 0; i < 13; i++) {
-        //     data[i] += 1.0;
-        // }
-
-        // latitude = latitude + 0.00001;
-        // longitude = longitude + 0.00001;
-        // memcpy(dataPacket, data, sizeof(float) * 13);
-        // memcpy(dataPacket + sizeof(float) * 13, &latitude, sizeof(float));
-        // memcpy(dataPacket + sizeof(float) * 13 + sizeof(float), &longitude, sizeof(float));
-        // pModuleLoRa->radioTX(dataPacket, dataPacketSize);
-        // delay(250);
-
-    //--------ECRAN------------
-
-    //     if(etat_PIT) {PIT_LCD();} //Si on reçoit alarme du stand (supervions) affichage du mot PIT sur l'écran
-    //     else {
-    //         SPEEDCOUNTER(speed);                                  //Gestion affichage vitesse
-    //         TOPSPEED(speed);                                      //Gestion vitesse maximum
-    //         BATTERY(tension_big_battery, tension_small_battery);  //Gestion batterie
-    //         CHRONOMETER();                                        //Gestion chronomètre
-
-    //         etat_clean = true;
-
-    //         //A utiliser pour phase de test
-    //         /*
-    //         speed++;
-    //         delay(200);
-    //         */
-    //     }
-
-    //    float temperature = temperatureSensor.readTemperature(); // Lit la température du capteur
-    //    Serial.print("Temperature: ");
-    //    Serial.print(temperature);
-    //    Serial.println(" C");
+    #if Activate_Temperature == 1
+    float temperature = LectTemp.readTemperature(); // Lit la température du capteur
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+    #endif
     
-    //  MFreq.loop();
-    //  float freq = MFreq.getFreq();
+    #if Activate_ACCEL_FREIN == 1
+    Serial.print("Frein : ");
+    Serial.println(lesLectures.readFrein());
+    Serial.print("Accel : ");
+    Serial.println(lesLectures.readAccel());
+    delay(500);
+    Serial.println(lectureFreinAccel.getFr_Prcent());
+    Serial.println(lectureFreinAccel.getAc_Prcent());
+    #endif
+
+    #if Activate_LoRa == 1
+    for (int i = 0; i < 13; i++) {
+        data[i] += 1.0;
+    }
+    latitude = latitude + 0.00001;
+    longitude = longitude + 0.00001;
+    memcpy(dataPacket, data, sizeof(float) * 13);
+    memcpy(dataPacket + sizeof(float) * 13, &latitude, sizeof(float));
+    memcpy(dataPacket + sizeof(float) * 13 + sizeof(float), &longitude, sizeof(float));
+    pModuleLoRa->radioTX(dataPacket, dataPacketSize);
+    delay(250);
+    #endif
+
+    #if Activate_Ecran == 1 
+    if(etat_PIT) {PIT_LCD();} //Si on reçoit alarme du stand (supervions) affichage du mot PIT sur l'écran
+    else {
+        SPEEDCOUNTER(speed);                                  //Gestion affichage vitesse
+        TOPSPEED(speed);                                      //Gestion vitesse maximum
+        BATTERY(tension_big_battery, tension_small_battery);  //Gestion batterie
+        CHRONOMETER();                                        //Gestion chronomètre
+
+        etat_clean = true;
+
+        //A utiliser pour phase de test
+        /*
+        speed++;
+        delay(200);
+        */
+    }
+    #endif
+    
+    #if Activate_FREQ == 1
+    MFreq.loop();
+    float freq = MFreq.getFreq();
+    #endif
 }
